294e4f7193f96c24e53fb117659491a3a2e223ef
UCHIYAMA Ryota <lessironglance@gmail.com>
2025-07-30 08:23:23 +0900

    update: add sample app

---
+++ sample/Console/Program.cs
@@ -0,0 +1,22 @@
+﻿
+Console.WriteLine("input api key");
+var apiKey = Console.ReadLine();
+
+if (string.IsNullOrWhiteSpace(apiKey))
+{
+    Console.WriteLine("api key is invalid");
+    return;
+}
+
+Console.WriteLine("input model uuid");
+var uuid = Console.ReadLine();
+
+Console.WriteLine("input text");
+var text = Console.ReadLine();
+
+Aivis.AivisClientOptions options = new(apiKey!.Trim());
+Aivis.AivisClient aivisClient = new(options);
+var stream = await aivisClient.TTS(uuid.Trim(), text);
+
+Aivis.Speaker speaker = new();
+await speaker.PlayAsync(Aivis.MediaType.MP3, stream);
