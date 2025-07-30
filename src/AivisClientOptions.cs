eb42c2be1a6f4aaefa098c326b1fda254e782f59
UCHIYAMA Ryota <lessironglance@gmail.com>
2025-07-31 08:37:06 +0900

    update: add xml comment

--- src/AivisClientOptions.cs
+++ src/AivisClientOptions.cs
@@ -1,17 +1,33 @@
 namespace Aivis;
 
+/// <summary>
+/// AivisClientOptions クラスは、Aivis API クライアントの設定を管理します。
+/// </summary>
 public class AivisClientOptions
 {
-
+    /// <summary>
+    /// API キー。
+    /// </summary>
     internal string ApiKey { init; get; }
 
+    /// <summary>
+    /// API のベース URL。
+    /// </summary>
     public string BaseUrl { init; get; } = "https://api.aivis-project.com";
 
+    /// <summary>
+    /// 新しい AivisClientOptions クラスのインスタンスを初期化します。
+    /// </summary>
+    /// <param name="apiKey">API キー。</param>
     public AivisClientOptions(string apiKey)
     {
         ApiKey = apiKey;
     }
 
+    /// <summary>
+    /// AivisClientOptions のクローンを作成します。
+    /// </summary>
+    /// <returns>クローンされた AivisClientOptions。</returns>
     public AivisClientOptions Clone()
     {
         return new AivisClientOptions(ApiKey)
