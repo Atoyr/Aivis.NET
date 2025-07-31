6ce2ca1fd16dc04b0b58f9662b3f056787047f18
UCHIYAMA Ryota <lessironglance@gmail.com>
2025-07-31 09:58:36 +0900

    update: add while loop

--- sample/Console/Program.cs
+++ sample/Console/Program.cs
@@ -1,21 +1,26 @@
 ﻿Console.WriteLine("input api key");
-var apiKey = Console.ReadLine();
+string? apiKey;
+string? modelUuid;
+string? text;
 
-if (string.IsNullOrWhiteSpace(apiKey))
+while (string.IsNullOrWhiteSpace(apiKey = Console.ReadLine()))
 {
     Console.WriteLine("api key is invalid");
-    return;
 }
 
-Console.WriteLine("input model uuid");
-var uuid = Console.ReadLine();
+while (string.IsNullOrWhiteSpace(modelUuid = Console.ReadLine()))
+{
+    Console.WriteLine("model uuid is invalid");
+}
 
-Console.WriteLine("input text");
-var text = Console.ReadLine();
+while (string.IsNullOrWhiteSpace(text = Console.ReadLine()))
+{
+    Console.WriteLine("text is invalid");
+}
 
 Aivis.AivisClientOptions options = new(apiKey!.Trim());
 Aivis.AivisTTSClient ttsCient = new(options);
-var stream = await ttsCient.Synthesize(uuid!.Replace("\n", "").Trim(), text);
+var stream = await ttsCient.Synthesize(modelUuid!.Replace("\n", "").Trim(), text!);
 
 Aivis.Speaker speaker = new();
 await speaker.PlayAsync(Aivis.MediaType.MP3, stream);
