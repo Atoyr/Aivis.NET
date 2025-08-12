a8b618b166d5edcc4dfe4f136d5591ee44b671aa
UCHIYAMA Ryota <lessironglance@gmail.com>
2025-08-12 21:45:48 +0900

    update: MP3Speaker

--- sample/StreamPlay/Program.cs
+++ sample/StreamPlay/Program.cs
@@ -22,7 +22,7 @@ Aivis.AivisClientOptions options = new(apiKey!.Trim());
 Aivis.AivisTTSClient ttsClient = new(options);
 using var stream = await ttsClient.SynthesizeStreamAsync(modelUuid!.Replace("\n", "").Trim(), text!);
 
-Aivis.Speaker speaker = new();
+Aivis.Speakers.ISpeaker speaker = new Aivis.Speakers.MP3Speaker();
 Console.WriteLine("チャンクの受信を開始します...");
 Console.WriteLine("ストリーミング再生を開始します... ");
-await speaker.PlayAsync(Aivis.MediaType.MP3, stream);
+await speaker.PlayAsync(stream);
